da592988364cda5aa5b31d31808679ba2dfbde6b
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-03-24 23:30:06 +0800

    first commit

---
+++ public/first.js
@@ -0,0 +1,16 @@
+"use strict";
+const ninje = {
+    name: "lazmi"
+};
+let char;
+let age;
+let isLoggedIn;
+let arr = [];
+arr.push("he");
+let birdName = 25;
+let hello = "";
+let greet;
+greet = (name) => {
+    console.log(name + "says");
+};
+greet("lazmi");
---
+++ src/first.ts
@@ -0,0 +1,24 @@
+
+
+const ninje ={
+     name : "lazmi"
+}
+
+let char :string;
+let age : number;
+let isLoggedIn : boolean;
+let arr : string []=[];
+arr.push("he");
+
+let birdName :any =25;
+let hello:string ="";
+
+let greet :Function;
+
+greet =(name: string)=>{
+     console.log(name +"says");
+}
+greet("lazmi");
+
+
+
